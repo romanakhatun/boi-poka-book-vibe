b107197b48a69c576c5638801ba5a142d87f7062
romanakhatun <romanakhatun2004@gmail.com>
2025-10-07 18:26:32 +0600

    book details

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,3 +1,4 @@
+import { FaRegStar } from "react-icons/fa6";
 import { useLoaderData, useParams } from "react-router";
 
 const BookDetails = () => {
@@ -7,7 +8,60 @@ const BookDetails = () => {
   const singleBook = data.find((book) => book.bookId === bookId);
   console.log(singleBook);
 
-  return <div></div>;
+  const { bookName, author, image, rating, tags, review } = singleBook;
+
+  return (
+    <div className="flex flex-col md:flex-row gap-12 p-5">
+      <figure className="bg-[#f3f3f3] p-[74px] rounded-xl">
+        <img className="w-48" src={image} alt={bookName} />
+      </figure>
+      <div className="flex-1">
+        <h2 className="card-title font-playfair font-bold">{bookName}</h2>
+        <p className="font-medium text-[#131313cc] mt-4 mb-5 ">By: {author}</p>
+        <div className="border-b-[1px] border-[#13131326]"></div>
+        <p className="text-[16px] my-4">Fiction</p>
+        <div className="border-b-[1px] border-[#13131326]"></div>
+        <p className="mt-4">
+          <strong>Review: </strong>
+          {review}
+        </p>
+        <div className="flex gap-8 mt-[31px] mb-[23px] ">
+          <strong>Tag</strong>
+          {tags.map((tag, i) => (
+            <li key={i} className="list-none text-[#23BE0A] font-medium">
+              #{tag}
+            </li>
+          ))}
+        </div>
+        <div className="border-b-[1px] border-[#13131326] my-4"></div>
+
+        <div className="text-[#131313cc] space-y-3">
+          <div className="flex items-center gap-2">
+            <p>Number of Pages:</p>
+            <span className="text-[16px] mt-1 font-semibold">{rating}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Publisher:</p>
+            <span className="text-[16px] mt-1 font-semibold">{rating}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Year of Publishing:</p>
+            <span className="text-[16px] mt-1 font-semibold">{rating}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Rating:</p>
+            <span className="text-[16px] mt-1 font-semibold">{rating}</span>
+          </div>
+        </div>
+        <div className="space-x-4 mt-8">
+          <button className="btn bg-white">Read</button>
+          <button className="btn bg-[#50B1C9] border-none text-white">
+            Wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookDetails;
